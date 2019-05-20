cb3875ab73167381f68c170348fb1d9a441558be
Douglas Kapocsi <50532033+Maliboa@users.noreply.github.com>
2019-05-20 16:00:06 -0400

    Update getMonthlyTempData.php
    This scripts pulls 30 days of 5 minute interval temperature data from a weather station database and outputs a JSON array for use with the Google Visualizations API.

--- getMonthlyTempData.php
+++ getMonthlyTempData.php
@@ -1,14 +1,18 @@
 <?php
-	
-	$dbServername = "127.0.0.1";
-	$dbUsername = "maliboa";
-	$dbPassword = "1464572";
-	$dbName = "Test_WS";
+	//Enter the database creditials here
+	$dbServername = "###.####.###.###";
+	$dbUsername = "yourUsername";
+	$dbPassword = "yourPassword";
+	$dbName = "yourDBName";
 	
 	$conn =  mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-	$sql = "SELECT id, temp FROM Data ORDER BY id DESC LIMIT 60480";
+	
+	//This is the SQL query for 30 days of 5 minute intervals, change the number after LIMIT as needed
+	$sql = "SELECT [primaryKeyName], [temperatureColumnName] FROM [yourDBName] ORDER BY [primaryKeyName] DESC LIMIT 60480";
 	
 	$result = mysqli_query($conn, $sql);
+	
+	//building the PHP array
 	$rows = array();
 	$table = array();
 	
@@ -23,6 +27,7 @@
 			)
 	);
 	
+	// populating the $rows array
 	while($row = mysqli_fetch_array($result)){
 		
 		$sub_array = array();
@@ -31,6 +36,7 @@
 		$rows[] = array('c' => $sub_array);	
 	}
 	
+	//populating the $table array and encoding as a JSON array
 	$table['rows'] = $rows;
 	$jsonTable = json_encode($table);
 
